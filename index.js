d1c29a7e2e7db276be0a3941d4fc8b93a6b5883b
anirudh10sharma <anirudhsharma.cse18@chitkarauniversity.edu.in>
2021-04-18 15:36:22 +0530

    com

--- index.js
+++ index.js
@@ -100,7 +100,7 @@ passport.use(
       {
         clientID: "784287410700-2krvqh92djat38ma05aetjkj5an5lbu8.apps.googleusercontent.com",
         clientSecret: "PvaHmQrasjmpITdZS248KRcR",
-        callbackURL: "http://iftwcblog.herokuapp.com/auth/google/callback",
+        callbackURL: "/auth/google/callback",
         proxy: true
       },
       (accessToken, refreshToken, profile, done) => {
